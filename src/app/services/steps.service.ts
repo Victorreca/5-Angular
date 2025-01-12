6bfbbe5a50e217067f510b9ab636c30697d33069
Victor Redondo <vrviktor@gmail.com>
2025-01-12 11:55:55 +0100

    Add steps service

--- src/app/services/steps.service.ts
+++ src/app/services/steps.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { IStep } from '../interfaces/i-step';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class StepsService {
+  steps: IStep[] = [
+    {
+      title: 'Dedica moltes hores',
+      description:
+        'Un mínim de 30 hores a la setmana. Si no en tens prou, hauràs de dedicar-li més hores. Al principi sembla impossible, però notaràs una millora ràpidament.',
+      img: '',
+      bgcolor: '#f5f5f5',
+    },
+    {
+      title: 'Programa projectes propis',
+      description:
+        'Més val 10 hores treballant en projectes propis, que 10 hores mirant tutorials. La motivació i la implicació en el projecte ajudarà a accelerar el teu aprenentatge.',
+      img: '',
+      bgcolor: '#e0e0e0',
+    },
+    {
+      title: 'Procura descansar',
+      description:
+        "Descansar bé i desconnectar són vitals. D'aquesta manera reduiràs l'estrès i l'ansietat. Milloraràs la teva concentració i consolidaràs el teu aprenentatge.",
+      img: '',
+      bgcolor: '#d6d6d6',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 }
