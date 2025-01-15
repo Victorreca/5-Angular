901202459a24295eb3bc40b1ad0525a3c678a73e
Victor Redondo <vrviktor@gmail.com>
2025-01-15 12:45:18 +0100

    Add animation carousel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
