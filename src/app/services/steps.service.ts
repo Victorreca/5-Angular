c57b31d8901f766f4d8a5bf91bf036f88586f80f
Victor Redondo <vrviktor@gmail.com>
2025-01-16 10:25:41 +0100

    Move components folders

--- src/app/services/steps.service.ts
+++ src/app/services/steps.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { IStep } from '../interfaces/i-step';
+import { IStep } from '../interfaces/i-step.model';
 
 @Injectable({
   providedIn: 'root',
